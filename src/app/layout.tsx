0e4ba0a8a6fb154995c1ef86904ea32bec549c62
amir-op <amiropcode@outlook.com>
2024-07-17 09:28:50 -0700

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import "~/styles/globals.css";
-import localFont from "next/font/local";
 import { type Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import "~/styles/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
