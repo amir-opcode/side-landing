48bd9b61641ec163c1037b7716cd4206321f1a85
amir-op <amiropcode@outlook.com>
2024-07-17 01:55:55 -0700

    feat; update next config file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,22 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "file-loader",
+      },
+    });
+
+    return config;
+  },
+};
 
 export default config;
